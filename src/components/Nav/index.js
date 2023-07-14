de05623525bf60235281f2311f3ba5ee88cb5911
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-14 11:15:04 -0700

    added Nav component tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -20,16 +20,16 @@ const Nav = () => {
     };
 
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a data-testid="about" href="#about">
                             About Me
                         </a>
                     </li>
