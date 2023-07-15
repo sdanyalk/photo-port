9b7434ca2d5d01ef14b8623097a8399bc3e3c3f0
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-15 13:06:53 -0700

    added state to contact form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const categories = [
@@ -34,6 +35,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContactForm = () => {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const { name, email, message } = formState;
+
+    const handleChange = (e) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(formState);
+    };
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name'></input>
+                    <input type='text' name='name' defaultValue={name} onChange={handleChange}></input>
                 </div>
                 <div>
                     <label htmlFor='email'>Email Address:</label>
-                    <input type='email' name='email'></input>
+                    <input type='email' name='email' defaultValue={email} onChange={handleChange}></input>
 
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows={5}></textarea>
+                    <textarea name='message' rows={5} defaultValue={message} onChange={handleChange}></textarea>
                 </div>
                 <button type='submit'>Submit</button>
             </form>
