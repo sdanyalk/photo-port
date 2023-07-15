5bfd28e3cc4985bbbe3b2c1b82df30c41c404a01
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-15 12:52:25 -0700

    added markup for contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ContactForm = () => {
+    return (
+        <section>
+            <h1>Contact Me</h1>
+            <form id='contact-form'>
+                <div>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' name='name'></input>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email Address:</label>
+                    <input type='email' name='email'></input>
+
+                </div>
+                <div>
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' rows={5}></textarea>
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    );
+};
+
+export default ContactForm;
