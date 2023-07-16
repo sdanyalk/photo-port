44def74bb2e1308887737d6578deaa727145dbd3
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-15 18:19:41 -0700

    added conditional rendering

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 const ContactForm = () => {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
 
     const { name, email, message } = formState;
 
     const handleChange = (e) => {
-        setFormState({ ...formState, [e.target.name]: e.target.value });
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+
+            if (!isValid) {
+                setErrorMessage('Your email is invalid');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
     };
 
     const handleSubmit = (e) => {
@@ -25,7 +44,7 @@ const ContactForm = () => {
                 </div>
                 <div>
                     <label htmlFor='email'>Email Address:</label>
-                    <input type='email' name='email' defaultValue={email} onChange={handleChange}></input>
+                    <input type='email' name='email' defaultValue={email} onBlur={handleChange}></input>
 
                 </div>
                 <div>
@@ -33,6 +52,11 @@ const ContactForm = () => {
                     <textarea name='message' rows={5} defaultValue={message} onChange={handleChange}></textarea>
                 </div>
                 <button type='submit'>Submit</button>
+                {errorMessage && (
+                    <div>
+                        <p className='error-text'>{errorMessage}</p>
+                    </div>
+                )}
             </form>
         </section>
     );
